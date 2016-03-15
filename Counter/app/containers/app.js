c45a9bd2d03c9f4c7e42f4b33e6e2f0998b9f684
dmoon <dmoon.t@gmail.com>
2016-03-15 18:33:49 +0800

    init app.js

--- Counter/app/containers/app.js
+++ Counter/app/containers/app.js
@@ -1,20 +1,48 @@
-import React, { Component } from 'react-native';
+import React, { Component, Navigator, StyleSheet } from 'react-native';
+import RNRF, {Route, Schema, Animations, Actions, TabBar} from 'react-native-router-flux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import * as reducers from '../reducers';
-import CounterApp from './counterApp';
+// import AppRoutes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
 const store = createStoreWithMiddleware(reducer);
+// let store = createStore(reducer);
+
+const Router = connect()(RNRF.Router);
+
+// {Router, routerReducer, Route, Container, Animations, Schema}
+import ReactNativeSimpleAuth from './loginApp';
+import CounterApp from './counterApp';
+
+class AppRoutes extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+  }
+
+  render() {
+    // const { state, actions } = this.props;
+    return (
+        <Router hideNavBar={true} name="root">
+            <Schema name="default" sceneConfig={Navigator.SceneConfigs.FloatFromRight} />
+            <Route name="Login" component={ReactNativeSimpleAuth} initial={true} title="Login" />
+            <Route name="Camera" component={CounterApp} title="Camera" />
+        </Router>
+    );
+  }
+}
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <CounterApp />
+        <AppRoutes />
       </Provider>
     );
   }
