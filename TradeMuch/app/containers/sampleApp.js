5eeb2df540e8faeff9c30b5cac55aa3d5232815c
dmoon <dmoon.t@gmail.com>
2016-03-18 00:45:31 +0800

    clean code

--- TradeMuch/app/containers/sampleApp.js
+++ TradeMuch/app/containers/sampleApp.js
@@ -1,41 +1,25 @@
-/**
-* Copyright (c) 2015-present, Facebook, Inc. All rights reserved.
-*
-* You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
-* copy, modify, and distribute this software in source code or binary form for use
-* in connection with the web services and APIs provided by Facebook.
-*
-* As with any software that integrates with the Facebook platform, your use of
-* this software is subject to the Facebook Developer Principles and Policies
-* [http://developers.facebook.com/policy/]. This copyright notice shall be
-* included in all copies or substantial portions of the software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-'use strict';
-
-var React = require('react-native');
-var {
+import React, {
   AppRegistry,
   StyleSheet,
   Text,
   View,
   Image,
   TouchableOpacity
-} = React;
-import {Actions} from 'react-native-router-flux';
-var Login = require('../components/login');
+} from 'react-native';
+import {
+  Actions
+} from 'react-native-router-flux';
+import { connect } from 'react-redux';
+
+
+import FBLogin from '../components/login';
+
+let styles = StyleSheet.create(require('./styles.js'));
 
 /**
  * A sample app that demonstrates use of the FBSDK login button, native share dialog, and graph requests.
  */
-export default class NHSample extends React.Component {
+class Login extends React.Component {
   render() {
     return (
       <Image
@@ -46,10 +30,18 @@ export default class NHSample extends React.Component {
             <Text style={styles.disclaimerText}>Images taken from New Horizons Facebook page</Text>
           </TouchableOpacity>
         </View>
-        <Login style={styles.loginContainer}/>
+        <FBLogin style={styles.loginContainer}/>
       </Image>
     );
   }
 }
 
-var styles = StyleSheet.create(require('./styles.js'));
+
+function _injectPropsFromStore(state) {
+  return state
+};
+
+const _injectPropsFormActions = {
+};
+
+export default connect(_injectPropsFromStore, _injectPropsFormActions)(Login);
