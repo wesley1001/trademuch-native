67c56333b7fa8257924bd9efbf54f15d7c1e9d30
dmoon <dmoon.t@gmail.com>
2016-03-18 00:44:28 +0800

    add auth actions & reducer

--- TradeMuch/app/components/login.js
+++ TradeMuch/app/components/login.js
@@ -1,87 +1,77 @@
-/**
-* Copyright (c) 2015-present, Facebook, Inc. All rights reserved.
-*
-* You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
-* copy, modify, and distribute this software in source code or binary form for use
-* in connection with the web services and APIs provided by Facebook.
-*
-* As with any software that integrates with the Facebook platform, your use of
-* this software is subject to the Facebook Developer Principles and Policies
-* [http://developers.facebook.com/policy/]. This copyright notice shall be
-* included in all copies or substantial portions of the software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
 
-'use strict';
-
-var React = require('react-native');
-var {
+import React, {
   StyleSheet,
-  View,
-} = React;
+  View
+} from 'react-native';
 
-var FBSDKLogin = require('react-native-fbsdklogin');
-var {
-  FBSDKLoginButton,
-} = FBSDKLogin;
-var FBSDKCore = require('react-native-fbsdkcore');
-var {
-  FBSDKAccessToken,
-} = FBSDKCore;
+import { connect } from 'react-redux';
 
-var Login = React.createClass({
-  render: function() {
-    return (
-      <View style={this.props.style}>
-        <FBSDKLoginButton
-          style={styles.loginButton}
-          onWillLogin={() => {
-            FBSDKAccessToken.getCurrentAccessToken((result) => {
-              console.log('token',result);
-              if (result == null) {
-                console.log('access token', result);
-                alert('Start logging in.');
-              } else {
-                alert('Start logging out.');
-              }
-            });
-            return true;
-          }}
-          onLoginFinished={(error, result) => {
-            if (error) {
-              alert('Error logging in.');
-            } else {
-              if (result.isCancelled) {
-                alert('Login cancelled.');
-              } else {
-                console.log('login result', result);
-                FBSDKAccessToken.getCurrentAccessToken((result) => {
-                  console.log('token', JSON.stringify(result,null,4));
-                  var token = result;
-                  if (result == null) {
-                    alert('Start logging in.');
-                  } else {
-                    alert('Start logging out.');
-                  }
-                });
-                alert('Logged in.');
-              }
-            }
-          }}
-          onLogoutFinished={() => alert('Logged out.')}
-          readPermissions={[]}
-          publishPermissions={[]}/>
-      </View>
-    );
-  }
-});
+import FBSDKLogin, {
+  FBSDKLoginButton
+} from 'react-native-fbsdklogin';
+
+import FBSDKCore, {
+  FBSDKAccessToken
+} from 'react-native-fbsdkcore';
+
+import {
+  requestUserInfo
+} from '../actions/authActions'
 
 var styles = StyleSheet.create(require('./styles.js'));
 
-module.exports = Login;
+export default class Login extends React.Component{
+  render() {
+    console.log(this.props);
+    return (
+      <View style={this.props.style}>
+      <FBSDKLoginButton
+        style={styles.loginButton}
+        onWillLogin={() => {
+          FBSDKAccessToken.getCurrentAccessToken((result) => {
+            // console.log('token',result);
+            if (result == null) {
+              // alert('Start logging in.');
+            } else {
+              // alert('Start logging out.');
+            }
+          });
+          return true;
+        }}
+        onLoginFinished={(error, result) => {
+          if (error) {
+            alert('Error logging in.');
+          } else {
+            if (result.isCancelled) {
+              // alert('Login cancelled.');
+            } else {
+              FBSDKAccessToken.getCurrentAccessToken(async userIdentities => {
+                this.props.requestUserInfo(userIdentities);
+                if (result == null) {
+                  // alert('Start logging in.');
+                } else {
+                  // alert('Start logging out.');
+                }
+              });
+              // alert('Logged in.');
+            }
+          }
+        }}
+        onLogoutFinished={() => {}}
+        readPermissions={[]}
+        publishPermissions={[]}/>
+      </View>
+    );
+  }
+};
+
+function _injectPropsFromStore(state) {
+  return {
+  }
+};
+
+const _injectPropsFormActions = {
+  requestUserInfo
+};
+
+export default connect(_injectPropsFromStore, _injectPropsFormActions)(Login);
