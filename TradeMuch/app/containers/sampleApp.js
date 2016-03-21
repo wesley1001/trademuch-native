9103b823f1a5cfe0005ad53c0aa83aeef6a6d823
dmoon <dmoon.t@gmail.com>
2016-03-21 11:50:03 +0800

    eslint sampleApp.js

--- TradeMuch/app/containers/sampleApp.js
+++ TradeMuch/app/containers/sampleApp.js
@@ -1,48 +1,43 @@
 import React, {
-  AppRegistry,
   StyleSheet,
   Text,
   View,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
 import {
-  Actions
+  Actions,
 } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 
 
 import FBLogin from '../components/login';
 
-let styles = StyleSheet.create(require('./styles.js'));
+const styles = StyleSheet.create(require('./styles.js'));
 
 /**
- * A sample app that demonstrates use of the FBSDK login button, native share dialog, and graph requests.
+ * A sample app that demonstrates use of the FBSDK login button ,
+ * native share dialog, and graph requests.
  */
-class Login extends React.Component {
-  render() {
-    return (
-      <Image
-        source={{uri: 'plutoBack.png'}}
-        style={styles.loginImage}>
-        <View style={styles.disclaimerContainer}>
-          <TouchableOpacity onPress={Actions.Camera}>
-            <Text style={styles.disclaimerText}>Images taken from New Horizons Facebook page</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={Actions.PostList}>
-            <Text style={styles.disclaimerText}>List Test Page</Text>
-          </TouchableOpacity>
-        </View>
-        <FBLogin style={styles.loginContainer}/>
-      </Image>
-    );
-  }
+function Login() {
+  return (
+    <Image source={{ uri: 'plutoBack.png' }} style={styles.loginImage}>
+      <View style={styles.disclaimerContainer}>
+        <TouchableOpacity onPress={Actions.Camera}>
+          <Text style={styles.disclaimerText}>Images taken from New Horizons Facebook page</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={Actions.PostList}>
+          <Text style={styles.disclaimerText}>List Test Page</Text>
+        </TouchableOpacity>
+      </View>
+      <FBLogin style={styles.loginContainer} />
+    </Image>
+  );
 }
 
-
 function _injectPropsFromStore(state) {
-  return state
-};
+  return state;
+}
 
 const _injectPropsFormActions = {
 };
