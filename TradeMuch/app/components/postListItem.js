66b0e6bdedb54a27c77e2a9637696b6d0fc7273c
fuyaode <dan82625@gmail.com>
2016-03-21 14:33:10 +0800

    fix app/components/PostListItem.js code style

--- TradeMuch/app/components/postListItem.js
+++ TradeMuch/app/components/postListItem.js
@@ -1,15 +1,45 @@
 import React, {
   StyleSheet,
   Component,
-  Dimensions,
   PixelRatio,
   View,
   Text,
   TouchableOpacity,
-  TouchableHighlight,
-  ListView,
-  Image
+  Image,
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  commentContent: {
+    padding: 10,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  userName: {
+    fontWeight: '700',
+  },
+  commentBody: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  commentText: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  cellBorder: {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    // Trick to get the thinest line the device can display
+    height: 1 / PixelRatio.get(),
+    marginLeft: 4,
+  },
+  avatar: {
+    borderRadius: 3,
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+});
 
 export default class PostListItem extends Component {
   constructor(props) {
@@ -18,7 +48,7 @@ export default class PostListItem extends Component {
       title: this.props.title,
       description: this.props.description,
       uri: this.props.uri,
-    }
+    };
   }
 
   componentWillMount() {
@@ -30,8 +60,7 @@ export default class PostListItem extends Component {
         <TouchableOpacity underlayColor={"#f3f3f3"}>
           <View>
             <View style={styles.commentContent}>
-                <Image source={{uri: this.state.uri}}
-                       style={styles.avatar}/>
+                <Image source={{ uri: this.state.uri }} style={styles.avatar} />
               <View style={styles.commentBody}>
                 <Text style={styles.userName}>
                   {this.state.title}
@@ -49,41 +78,14 @@ export default class PostListItem extends Component {
   }
 }
 
+PostListItem.propTypes = {
+  title: React.PropTypes.string,
+  description: React.PropTypes.string,
+  uri: React.PropTypes.string,
+};
+
 PostListItem.defaultProps = {
   title: '',
   description: '',
   uri: 'https://unsplash.it/200/300/?random',
 };
-
-const styles = StyleSheet.create({
-  commentContent: {
-    padding: 10,
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "flex-start"
-  },
-  userName: {
-    fontWeight: "700"
-  },
-  commentBody: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center"
-  },
-  commentText: {
-    flex: 1,
-    flexDirection: "row"
-  },
-  cellBorder: {
-    backgroundColor: "rgba(0, 0, 0, 0.2)",
-    // Trick to get the thinest line the device can display
-    height: 1 / PixelRatio.get(),
-    marginLeft: 4,
-  },
-  avatar: {
-    borderRadius: 3,
-    width: 40,
-    height: 40,
-    marginRight: 10
-  }
-});
