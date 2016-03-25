ada977ef754cd9d9b0ea82e23dc1626aab7f241f
dmoon <dmoon.t@gmail.com>
2016-03-25 14:40:03 +0800

    merge check

--- TradeMuch/app/routes.js
+++ TradeMuch/app/routes.js
@@ -8,7 +8,7 @@ const Router = connect()(RNRF.Router);
 // View
 import PostList from './containers/PostList';
 import PostDetail from './containers/PostDetail';
-import MessengerSample from './components/Messenger/Messenger';
+import Messenger from './containers/Messenger';
 import Login from './components/Login';
 // import NHSample from './containers/sampleApp';
 
@@ -16,9 +16,9 @@ export default function AppRoutes() {
   return (
     <Router name="root">
       <Schema name="default" sceneConfig={Navigator.SceneConfigs.FloatFromRight} />
-      <Route name="Login" component={MessengerSample} title="Login" />
+      <Route name="Messenger" component={Messenger} title="Messenger" initial />
       <Route name="PostList" component={PostList} title="PostList" type="replace" />
-      <Route name="PostDetail" component={PostDetail} title="PostDetail" initial />
+      <Route name="PostDetail" component={PostDetail} title="PostDetail" />
     </Router>
   );
 }
