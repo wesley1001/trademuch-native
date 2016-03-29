2c6f65d329dc30d79ca8c59ad4f1679e2b533f6d
dmoon <dmoon.t@gmail.com>
2016-03-29 14:22:56 +0800

    add receive userInfo case

--- TradeMuch/app/reducers/AuthReducer.js
+++ TradeMuch/app/reducers/AuthReducer.js
@@ -1,15 +1,15 @@
 import {
-  // RECEIVED_USER_INFO,
+  RECEIVED_USER_INFO,
   UPDATE_LOGIN_STATUS,
 } from '../actions/AuthActions';
 
 export function auth(state = { isLogin: false }, action) {
   switch (action.type) {
-    // case RECEIVED_USER_INFO:
-    //   return {
-    //     ...state,
-    //     userInfo: action.data,
-    //   };
+    case RECEIVED_USER_INFO:
+      return {
+        ...state,
+        userInfo: action.data,
+      };
     case UPDATE_LOGIN_STATUS:
       return {
         isLogin: action.data,
