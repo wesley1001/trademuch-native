e3e3d00f3ab3bff75a98d1d901bb9649d06b8f06
dmoon <dmoon.t@gmail.com>
2016-04-06 18:52:23 +0800

    split store

--- TradeMuch/app/App.js
+++ TradeMuch/app/App.js
@@ -1,26 +1,9 @@
-import React, { Platform } from 'react-native';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import React from 'react-native';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-import promise from 'redux-promise';
-import createDevTools from 'remote-redux-devtools';
-
-import * as reducers from './reducers';
-
-const logger = createLogger();
-const devTools = createDevTools({
-  name: Platform.OS,
-  hostname: 'localhost',
-  port: 5678,
-});
-const createStoreWithMiddleware = compose(
-  applyMiddleware(thunk, promise, logger), devTools)(createStore);
-const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
-
-
+import { configureStore } from './store';
 import AppRoutes from './routes';
+
+const store = configureStore();
 
 export default function App() {
   return (
