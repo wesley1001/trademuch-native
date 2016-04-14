d1169d0dfa33469ad60d5d62a7990ea1c570b1d5
dmoon <dmoon.t@gmail.com>
2016-04-14 14:43:24 +0800

    lock to portrait

--- app/App.js
+++ app/App.js
@@ -1,14 +1,21 @@
 import React from 'react-native';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
+import Orientation from 'react-native-orientation';
 import AppRoutes from './routes';
 
 const store = configureStore();
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <AppRoutes />
-    </Provider>
-  );
+export default class App extends React.Component {
+
+  componentDidMount() {
+    Orientation.lockToPortrait();
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
+    );
+  }
 }
